8c522310ad91b74bd69b5f7a45fa37c90a1dad3b
Emerson Oliveira <emerson.520@outlook.com>
2026-09-17 10:05:20 -0300

    Aprendendo Object e ToString

---
+++ Modulo-1-Classes Atributos e Metodos/ControleEstoque/ControleEstoque/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace ControleEstoque;
+
+internal class Produto
+{
+    public string? Nome;
+    public double Preco;
+    public int Quantidade;
+
+    public double ValorTotalEmEstoque()
+    {
+        return Preco * Quantidade;
+    }    
+
+    public void AdicionarProduto(int quantidade)
+    {
+        Quantidade += quantidade;
+    }
+
+    public void RemoverProduto(int quantidade)
+    {
+        Quantidade -= quantidade;
+    }
+
+    public override string ToString()
+    {
+        return $"{Nome}, R$ {Preco.ToString("F2",CultureInfo.InvariantCulture)}, {Quantidade} unidades, Total: R$ {ValorTotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture)}";
+    }
+}
---
+++ Modulo-1-Classes Atributos e Metodos/ControleEstoque/ControleEstoque/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace ControleEstoque;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto produto = new Produto();
+
+        Console.WriteLine("Entre com os dados do produto: ");
+        Console.Write("Nome: ");
+        produto.Nome = Console.ReadLine();
+        Console.Write("Preco: ");
+        produto.Preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        Console.Write("Quantidade no estoque: ");
+        produto.Quantidade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Dados do produto: {produto}");
+
+        Console.ReadKey();
+    }
+}
